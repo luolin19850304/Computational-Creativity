28a5e99073213751f916c43804c1a07387903e56
Norbert Logiewa <norbertlogiewa96@gmail.com>
2019-04-10 18:59:48 +0100

    Add imgs

--- code/corpus.py
+++ code/corpus.py
@@ -9,26 +9,30 @@ from random import choices
 from os.path import join, isfile
 from os import listdir, getenv
 import re 
-from typing import Any, Dict, List, Tuple, Optional, Set, Match, Union
+from typing import Any, Dict, List, Tuple, Optional, Set, Match, Union, Iterator
 
 TOKENIZE_REGEX = re.compile(r"(?P<token>(?P<punc>[,;])|(?P<sentend>\.(\.\.)?|\?(\?\?)?|!(!!)?)|(?P<word>([A-Za-z]?[a-z]+|[IAO])(-[a-z]+)*('[a-z]{0,4})?)|(?P<dash>--?)|(?P<qt>[\"']))", re.A | re.S)
 
-SENT_ENDINGS = {'.', '?', '!', '...', '???', '!!!'};
+SENT_ENDINGS: Set[str] = {'.', '?', '!', '...', '???', '!!!'};
+SENT_ENDINGS_LIST: List[str] = list(SENT_ENDINGS)
+SENT_ENDINGS_WEIGHTS: List[float] = \
+                         [0.55, 0.2, 0.1, 0.05, 0.05, 0.05]
 
-def tokenize(txt: str) -> List[Match]:
-    global TOKENIZE_REGEX
-    tokens = []
-    for match in TOKENIZE_REGEX.finditer(txt):
-        tokens.append(match)
-    return tokens
+DONT_REPEAT_TAGS: Set[str] = {'sentend', 'punc', 'qt', 'dash'}
 
 # set loging level using $ LOG=info ./corpus.py
-logging.basicConfig(
-        level=eval(f'logging.{getenv("LOG", "debug").upper()}', locals(), globals()),
-        format='%(levelname)s %(message)s')
-log = logging.getLogger()
+logging.basicConfig(format='%(levelname)s %(message)s')
+log = logging.getLogger('Corpus Logger')
+log.setLevel(eval(f'logging.{getenv("LOG", "debug").upper()}', locals(), globals()))
+
+def tokenize(txt: str) -> Iterator[Match]:
+    global TOKENIZE_REGEX
+    return TOKENIZE_REGEX.finditer(txt)
+
 
 def make_index(xs: List[Any]) -> Tuple[List[Any], Dict[Any, int]]:
+    """Make index and reverse index.
+    """
     d: Dict[Any, int] = dict()
     a: List[Any] = []
     idx = 0
@@ -40,7 +44,11 @@ def make_index(xs: List[Any]) -> Tuple[List[Any], Dict[Any, int]]:
     return a, d
 
 def prettify(tokens: Union[Tuple[str, ...], List[str]]) -> str:
+    """Transform list of words to legible text suitable for human consumption.
+    """
     global SENT_ENDINGS
+    global SENT_ENDINGS_LIST
+    global SENT_ENDINGS_WEIGHTS
     global TOKENIZE_REGEX
     ms: List[Match] = list(TOKENIZE_REGEX.finditer(' '.join(tokens)))
     chunks = [ms[0].group(0)]
@@ -83,7 +91,7 @@ def prettify(tokens: Union[Tuple[str, ...], List[str]]) -> str:
             chunks.append(m.group(0))
 
     if chunks[-1] not in SENT_ENDINGS:
-        chunks.append(choices(list(SENT_ENDINGS))[0])
+        chunks.append(choices(population=SENT_ENDINGS_LIST, weights=SENT_ENDINGS_WEIGHTS)[0])
 
     if chunks[0][0].islower():
         chunks[0] = chunks[0][0].upper() + chunks[0][1:]
@@ -91,7 +99,7 @@ def prettify(tokens: Union[Tuple[str, ...], List[str]]) -> str:
     return ''.join(chunks)
 
 
-class Doc:
+class Corpus:
     def __init__(self, text: str, name: str = ''):
         self.text = text
         self.name = name
@@ -106,44 +114,48 @@ class Doc:
         # iterate over all words as word numers
         self._word_idxs: array = None
 
-        self._nword_counts: List[Dict[Tuple[int, ...], Dict[int, int]]] = \
-            [None for i in range(20)]
-
         self._nword_ps: List[Dict[Tuple[int, ...], Dict[int, float]]] = \
-            [None for i in range(20)]
+            [None for i in range(30)]
+
+        log.warn('make sure to call `.index()` to initialise')
 
     @staticmethod
     def files() -> List[str]:
+        """List of files in the corpus.
+        """
         return [f for f in listdir('./data') if isfile(join('./data', f))]
 
     @staticmethod
     def from_file(name: str, chunk_size=10, delta=2, max_n_errors=10):
-        if name.endswith('.txt'):
-            with open(f'./data/{name}', encoding='ascii', errors='ignore') as f:
-                nerrors = 0
-                chunks = []
-                step = chunk_size
-                try:
+        """Create Corpus from a single file name.
+
+        NOTE it will look for it in the corpus ie. ./data dir).
+        """
+        if not name.endswith('.txt'):
+            return Corpus.from_file(f'{name}.txt')
+        with open(f'./data/{name}', encoding='ascii', errors='ignore') as f:
+            nerrors = 0
+            chunks = []
+            step = chunk_size
+            try:
+                result = f.read(step)
+                while result:
+                    chunks.append(result)
+                    step *= delta
                     result = f.read(step)
-                    while result:
-                        chunks.append(result)
-                        step *= delta
-                        result = f.read(step)
-                except UnicodeDecodeError as e:
-                    nerrors += 1
-                    step = chunk_size
-                    log.warn(str(e))
-                    if nerrors >= max_n_errors:
-                        log.warn(f'failed to read file {name}')
-                return Doc(''.join(chunks), name)
-        else:
-            return Doc.from_file(f'{name}.txt')
+            except UnicodeDecodeError as e:
+                nerrors += 1
+                step = chunk_size
+                log.warn(str(e))
+                if nerrors >= max_n_errors:
+                    log.warn(f'failed to read file {name}')
+            return Corpus(''.join(chunks), name)
 
     @staticmethod
-    def from_all_files(max_n_files=1000, delta=2, chunk_size=10, max_n_errors=10):
-        log.info('reading from all files in ./data')
+    def from_all_files(max_n_files=100, delta=2, chunk_size=10, max_n_errors=10):
         texts = []
-        files = Doc.files()[:max_n_files]
+        files = Corpus.files()[:max_n_files]
+        log.info(f'reading from {len(files)} files in ./data')
         for fname in files:
             log.debug(f'reading from {fname}')
             with open(join('./data', fname), encoding='ascii', errors='ignore') as f:
@@ -166,43 +178,48 @@ class Doc:
                 texts.append(''.join(chunks))
         txt = '\n\n'.join(texts)
         log.info(f'done reading from all {len(files)} files (read {len(txt)} bytes)')
-        return Doc(txt, f'{len(texts)} texts from ./data')
+        return Corpus(txt, f'{len(texts)} texts from ./data')
 
-    def index_words(self) -> None:
+    def index(self) -> None:
+        """Initialise the Corpus.
+
+        Associate a natural number with each word (INDEX)
+        and with each such natural number a word (REV-INDEX).
+        """
         if self._idx_to_word is not None:
             log.debug('cache HIT, index already generated')
             return
 
-        tags = []
-        words = []
+        global DONT_REPEAT_TAGS
+
+        words: List[str] = []
 
         log.info('tokenizing')
 
-        for m in tokenize(self.text):
-            words.append(m.group('token')) # ie full match
-            for g in m.groupdict():
-                # extract tag
-                if g != 'token' and m.group(g) is not None:
-                    tags.append(g)
-                    break
-
-        def info(ptr: int, what: str) -> None:
-            log.debug(f'2x {what} in a row ("{words[ptr - 1]}" followed by "{words[ptr]}"), removing (pos {ptr} / {len(tags)})')
+        prev_tag: str = 'word'
 
         # pre-process 
         nremoved = 0
-        ptr = 1 # look behind by 1
-        while ptr < len(tags):
-            tag = tags[ptr]
-            prev_tag = tags[ptr - 1]
-            if tag == 'sentend' and prev_tag == 'sentend':
-                info(ptr, 'sentend')
-                tags.pop(ptr)
-                words.pop(ptr)
-                nremoved += 1
-            else:
-                ptr += 1
 
+        for m in tokenize(self.text):
+            tag: str = None
+            # extract tag
+            for g in m.groupdict():
+                if g != 'token' and m.group(g) is not None:
+                    tag = g
+                    break
+
+            # skip
+            if prev_tag == tag:
+                if tag in DONT_REPEAT_TAGS:
+                    nremoved += 1
+                    continue
+
+            # else add full match
+            words.append(m.group('token'))
+            prev_tag = tag
+
+        log.info(f'created {len(words)} tokens, ratio of nchars to ntokens = {len(self.text)/len(words):4.2f}')
         log.info(f'pre-processing removed {nremoved} tokens (corpus smaller by {nremoved / (nremoved + len(words)):6.4f})')
 
         idx_to_word, word_to_idx = make_index(words)
@@ -218,7 +235,9 @@ class Doc:
 
         del self.text  # dealloc
 
-    def nword_ps(self, n=2, min_freq=1, min_p=0.001, min_total=1) -> Dict[Tuple[int, ...], Dict[int, float]]:
+    def nword_ps(self, n=2, min_freq=2, min_p=0.0001, min_total=2) -> Dict[Tuple[int, ...], Dict[int, float]]:
+        """Compute probabilites for every n-gram and the word after.
+        """
         if n < 2:
             raise Exception(f'n MUST be >= 2')
 
@@ -229,6 +248,8 @@ class Doc:
         log.debug(f'cache MISS for {n}-word probabilities, generating')
 
         def nword_counts(self) -> Dict[Tuple[int, ...], Dict[int, int]]:
+            """Get counts of every n-tuple of words.
+            """
             d: Dict[Tuple[int, ...], Dict[int, int]] = dict()
 
             # look ahead 
@@ -241,61 +262,53 @@ class Doc:
                 else:
                     maybe_dict[next_w] = maybe_dict.get(next_w, 0) + 1
 
-            # memoize
-            self._nword_counts[n] = d
-
             return d
 
         ps: Dict[Tuple[int, ...], Dict[int, float]] = nword_counts(self)
 
-        entries: List[Tuple[int, ...]] = list(ps.keys())
-
-        ptr = 0
-
         npruned_dicts = 0
-        npruned_entries = 0
+        log.info(f'prunning {n}-gram probability dict (len = {len(ps)} dicts)')
 
-        log.info(f'prunning probability dict (len = {len(ps)} dicts)')
+        for word_seq in list(ps.keys()):
 
-        while ptr < len(entries):
-            entry: Tuple[int, ...] = entries[ptr]
-            counts: Dict[int, int] = ps[entry]
+            if len(ps[word_seq]) < min_total:
+                del ps[word_seq]
+                npruned_dicts += 1
+                continue
+
+            # prune using ABSOLUTE frequencies to save memory
+            counts: Dict[int, int] = {widx: count for widx, count in ps[word_seq].items() if count >= min_freq}
+
+            if len(counts) < min_total:
+                del ps[word_seq]
+                npruned_dicts += 1
+                continue
 
             total: int = sum(counts.values())
 
-            # prune to save memory
-            ptr2 = 0
-            next_words = list(counts.keys())
+            probs = counts
 
-            while ptr2 < len(next_words):
-                next_w: int = next_words[ptr2]
-                freq: int = counts[next_w]
-                p: float = freq / total
-                if freq < min_freq or p < min_p:
-                    log.debug(f'prunning, removed word {self._idx_to_word[next_w]} with freq = {freq} and p = {p:6.4f}')
-                    del counts[next_w]
-                    total -= freq
-                    npruned_entries += 1
-                ptr2 += 1
+            del counts
 
-            # now check again
-            total = sum(counts.values())
+            for k in probs:
+                probs[k] /= total
 
-            if total < min_total:
-                del ps[entry]
+            # prune using RELATIVE frequencies to save memory
+            probs = {widx: p for widx, p in probs.items() if p >= min_p}
+
+            if len(probs) < min_total:
+                del ps[word_seq]
                 npruned_dicts += 1
             else:
-                for next_w in counts:
-                    counts[next_w] /= total
-            ptr += 1
+                ps[word_seq] = probs
 
         self._nword_ps[n] = ps
 
-        log.info(f'pruned {npruned_dicts} dicts (reduction by {1 - (len(ps) / (len(ps) + npruned_dicts)):6.4f}) and {npruned_entries} entries')
+        log.info(f'pruned {npruned_dicts} dicts (reduction by {1 - (len(ps) / (len(ps) + npruned_dicts)):6.4f}, {len(ps)} entries left)')
 
         return ps
 
-    def word_ps(self, min_p=0.0001) -> array:
+    def word_ps(self) -> array:
         if self._word_ps is not None:
             return self._word_ps
 
@@ -333,10 +346,12 @@ class Doc:
                 log.warn(f'unrecognised word {words[i]} (unseen in corpus), replacing with a rand word')
                 word_idxs[i] = self.rand_word_idx
 
+        del words
+
         while len(word_idxs) < max_len:
             found = False
 
-            for i in range(n, 1, -1): # upper is exclusive
+            for i in range(n, 1, -1): # upper is exclusive so iterate up to n..2
 
                 slice: Tuple[int, ...] = tuple(word_idxs[-i:])
                 words: List[str] = []
@@ -355,10 +370,7 @@ class Doc:
                     # needs indexing
                     idxs = list(maybe.keys())
                     ps = maybe.values()
-                    rand_widx: int = choices(
-                            population=idxs,
-                            weights=ps,
-                            k=1)[0]
+                    rand_widx: int = choices(population=idxs, weights=ps, k=1)[0]
 
                     candidate_width: int = \
                             reduce(max, (len(cand) for cand in map(lambda idx: self._idx_to_word[idx], idxs)))
@@ -387,14 +399,14 @@ class Doc:
         return [self._idx_to_word[idx] for idx in word_idxs]
 
     def __repr__(self) -> str:
-        return f'Corpus {self.name}'
+        return f'Corpus {self.name}' + (' UINDEXED' if self._idx_to_word is None else '')
 
 
 if __name__ == '__main__':
     import cmd
     log.info('running as a script')
-    d = Doc.from_all_files()
-    d.index_words()
+    d = Corpus.from_all_files()
+    d.index()
 
     n = 5
     max_len = 200
@@ -429,4 +441,4 @@ if __name__ == '__main__':
 
     REPL().cmdloop()
 
-# vim:sw=4:ts=8:expandtab:foldmethod=indent:nu:
+# vim:sw=4:ts=8:expandtab:foldmethod=indent:nu:hlsearch:
